6d5145d651c97f227e92a34b39bf30327de2c3b9
Peter Ajtai <peter.ajtai@gmail.com>
2012-11-27 19:34:16 -0800

    adding grunt tasks, removing things

---
+++ grunt.js
@@ -0,0 +1,49 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        cp: {
+            temp: {
+                src : "application",
+                dest:"temp"
+            }
+        },
+
+        useref: {
+            // specify which files contain the build blocks
+            html: 'temp/**/*.html',
+            // explicitly specify the temp directory you are working in
+            temp: 'temp'
+        },
+
+        shell: {
+            jekyll: {
+                command: "jekyll --server",
+                stdout: true,
+                stderr: true,
+                execOptions: {
+                    cwd: './temp'
+                }
+            },
+
+            clean: {
+                command: "rm buttons.css responsive.min.css styles.css syntax.css",
+                stdout: true,
+                stderr: true,
+                execOptions: {
+                    cwd: './temp/css'
+                }
+            }
+        }
+    });
+
+    // plugin tasks
+    grunt.loadNpmTasks('grunt-useref');
+    grunt.loadNpmTasks('grunt-cp');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-css');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('build', 'cp:temp useref concat cssmin shell:clean shell:jekyll');
+};
