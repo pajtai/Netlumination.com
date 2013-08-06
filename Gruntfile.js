4cdb0a95b0980ada89ac2301abadb7fb909e6b7d
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-08-06 12:18:04 -0700

    adding watch, open, livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,12 @@ module.exports = function(grunt) {
 
     'user strict';
 
-    var port = 8000;
+    var port = 8000,
+        path = require('path'),
+        lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet,
+        folderMount = function folderMount(connect, point) {
+            return connect.static(path.resolve(point));
+        };
 
     // load all grunt tasks
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
@@ -30,30 +35,57 @@ module.exports = function(grunt) {
         jekyll: {
             options: {
                 src : 'application',
-                dest: 'build',
-                auto : true
+                dest: 'build'
             },
             server : {
                 options: {
                     server : true,
-                    watch: true,
                     server_port : port
                 }
             },
             build: { }
         },
 
+        watch: {
+            options: {
+                // Start a live reload server on the default port: 35729
+                livereload: true,
+                spawn: true
+            },
+            spec: {
+                files: [
+                    'application/**/*'
+                ],
+                tasks: [
+                    'jekyll:build'
+                ]
+            }
+        },
+
         open: {
             server: {
                 path: 'http://localhost:' + port
             }
-        }
+        },
+
+        connect: {
+            livereload : {
+                options : {
+                    port       : port,
+                    hostname: 'localhost',
+                    base       : './build/',
+                    middleware : function (connect, options) {
+                        return [lrSnippet, folderMount(connect, options.base)]
+                    }
+                }
+            }
+        },
     });
 
     grunt.registerTask("kickoff", function() {
        grunt.util.spawn("grunt", ["jekyll:server"]);
     });
-    grunt.registerTask('server', 'Deploy website on localhost', ['open:server', 'jekyll:server']);
+    grunt.registerTask('server', 'Deploy website on localhost', ['jekyll:build', 'connect:livereload', 'open:server','watch']);
     grunt.registerTask("deploy", "Deploy to gh-pages", ['jekyll:build', 'build_gh_pages']);
 
 };
